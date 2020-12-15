27cb58e02218a1347cf86434bce5072dcb4460ec
OlmLisa <lisa.oulmi77@gmail.com>
2020-12-15 12:20:32 +0100

    version 1 - implementation des struct

---
+++ struc.go
@@ -0,0 +1,71 @@
+package test // à remplacer par main
+
+import (
+	"os"
+	"time"
+)
+
+//Block
+
+type Block struct {
+	Index             int
+	PreviousHash      string
+	Data              string
+	Timestamp         time.Time
+	Difficulty        int
+	Hash              string
+	Nonce             int
+	TransactionsCount int
+	Transactions      []Transaction
+}
+
+type Blockchain struct {
+	blocks []*Block
+}
+
+func CreateBlock() Block {
+	var block Block
+
+	block.Timestamp = time.Now()
+	block.Nonce = 0
+
+	return block
+}
+
+// Transaction data structure
+type Transaction struct {
+	From     string
+	Hash     string
+	Value    string
+	Gas      uint64
+	GasPrice uint64
+	Nonce    uint64
+	To       string
+	Pending  bool
+}
+
+// TransferEthRequest data structure
+type TransferEthRequest struct {
+	PrivKey string
+	To      string
+	Amount  int64
+}
+
+// HashResponse data structure
+type HashResponse struct {
+	Hash string
+}
+
+// BalanceResponse data structure
+type BalanceResponse struct {
+	Address string
+	Balance string
+	Symbol  string
+	Units   string
+}
+
+// Error data structure
+type Error struct {
+	Code    uint64
+	Message string
+}
